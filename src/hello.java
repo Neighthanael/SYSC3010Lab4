8f7b1a90623dae3dc8a1c4f31875130d2860161c
nfohk <nfohk@LAPTOP-Neighthan>
2018-10-01 12:25:57 -0400

    methodB

---
+++ src/hello.java
@@ -0,0 +1,17 @@
+
+public class hello {
+
+	public static void main(String[] args) 
+	{
+		hello hello = new hello();
+		hello.methodA();
+		hello.methodB();
+		hello.methodC();
+	}
+
+	public void methodA() { }
+
+	public void methodB() { }
+
+	public void methodC() { }
+}
